908f71f0e2c9429488749300c08d7d315f0be1e8
HalfSeraphic <96750594+HalfSeraphic@users.noreply.github.com>
2023-03-09 19:50:39 -0600

    pruebas php

--- 01.php
+++ 01.php
@@ -1,4 +1,30 @@
-<?php include 'includes/header.php';
+<?php
+declare(strict_types = 1);
+include 'includes/header.php';
 
+//Definir clase
+class Producto { //Las clases no emplean parentesis, tienen un cuerpo que si
+    public $nombre;
+    public $precio;
+    public $disponible;
+
+    public function __construct(string $nombre, int $precio, bool $disponible) //Este es un magic method
+    {
+        $this->nombre = $nombre;
+        $this->precio = $precio;
+        $this->disponible = $disponible;
+    }
+}
+
+$producto = new Producto('Tablet', 200, true);
+echo "<pre>";
+var_dump($producto);
+echo "</pre>";
+
+
+$producto2 = new Producto('Monitor curvo', 300, true);
+echo "<pre>";
+var_dump($producto2);
+echo "</pre>";
 
 include 'includes/footer.php';
